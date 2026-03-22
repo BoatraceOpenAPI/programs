615ec8561037db1f027f7c034364d030205a04d9
shimomo <yuichi@shimomo.net>
2026-03-22 22:02:02 +0900

    docs: update comments

--- scraper.php
+++ scraper.php
@@ -32,7 +32,7 @@ if (empty($programs ?? [])) {
 }
 
 // 出走表データを JSON ファイルとして保存
-// 日付付きの JSON ファイルとして保存（例: docs/{version}/YYYY/YYYYMMDD.json）
+// 日付付きの JSON ファイルとして保存（例: docs/v3/2026/20260322.json）
 // 最新データとして today.json にも保存
 $saver = new ProgramSaver();
 $saver->save($programs, "docs/{$version}/" . $date->format('Y') . '/' . $date->format('Ymd') . '.json');
